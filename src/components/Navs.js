526115216b68bf662705ea1b26f7cb8ae07f440a
Artniz23 <artniz23@gmail.com>
2020-11-15 17:50:33 +0300

    finishing our project

---
+++ src/components/Navs.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { NavList, LinkStyled } from './Navs.styled';
+
+const LINKS = [
+  {
+    to: '/',
+    text: 'Home',
+  },
+  {
+    to: '/starred',
+    text: 'Starred',
+  },
+];
+
+const Navs = () => {
+  const location = useLocation();
+
+  return (
+    <div>
+      <NavList>
+        {LINKS.map((item, key) => {
+          return (
+            <li key={key}>
+              <LinkStyled to={item.to} className={item.to === location.pathname ? 'active' : ''}>
+                {item.text}
+              </LinkStyled>
+            </li>
+          );
+        })}
+      </NavList>
+    </div>
+  );
+};
+
+export default Navs;
